834f73258b55647bbc1b9f5c83b8aa7b9216b393
Sergejs Stecenko <sstecenko@evolution.com>
2020-11-19 19:55:57 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOIT App</title>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./styles/style.css">
+</head>
+<body>
+    
+    <h1>Welcome&nbspTo DOIT&nbspApp</h1>
+
+    <div class="container">
+        <form method="POST">
+            <input type="text" placeholder="Username" name="username">
+            <input type="email" placeholder="Email" name="email">
+            <input type="password" placeholder="Password" name="password">
+            <button id="login-button" type="submit">Login/Register</button>
+        </form>
+    </div>
+
+</body>
+</html>
