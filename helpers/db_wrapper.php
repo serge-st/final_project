3d9512a7355d62328d4acbb0a4f34c1df0cc780f
Sergejs Stecenko <sstecenko@evolution.com>
2020-11-20 15:57:19 +0200

    complete db wrapper

--- helpers/db_wrapper.php
+++ helpers/db_wrapper.php
@@ -1,12 +1,42 @@
 <?php
 
 class DB {
-    private static $hostname;
-    private static $user;
-    private static $password;
-    private static $database;
+    private static $connection;
+    
+    private static function openConnection(){
+        $hostname = 'localhost:3306';
+        $user = 'root';
+        $password = '';
+        $database = 'doit_app_db';
+        
+        self::$connection = new mysqli($hostname, $user, $password, $database);
 
-    private static $mysqli = new mysqli($hostname, $user, $password, $database);
-    
-    
+        if (self::$connection->connect_errno){
+            $error = "Connection Error: " . self::$connection->connect_errno . ": " . self::$connection->connect_error;
+            self::closeConnection();
+            exit($error);
+        }
+    }
+
+    private static function closeConnection(){
+        self::$connection->close();
+        self::$connection = null;
+    }
+
+    public static function run($sql){
+        if (self::$connection === null){
+            self::openConnection();
+        }
+
+        $response = self::$connection->query($sql);
+
+        if (self::$connection->error){
+            $error = self::$connection->error;
+            self::closeConnection();
+            exit("SQL Error: " . $error);
+        }
+
+        self::closeConnection();
+        return $response;
+    }    
 }
