37a714cfd725bb4acd36efc5ee162b2db47270f9
Sergejs Stecenko <sstecenko@evolution.com>
2020-11-27 14:21:14 +0200

    small changes

--- api/test_api.php
+++ api/test_api.php
@@ -5,7 +5,7 @@ require_once __DIR__ . "/../helpers/db_wrapper.php";
 $userId = $_GET["user_id"];
 
 // method to use with list display:
-$result = DB::run("select * from `user_tasks` where user_id = '$userId'")->fetch_all(MYSQLI_ASSOC);
+$result = DB::run("SELECT * FROM `user_tasks` WHERE `user_id` = '$userId'")->fetch_all(MYSQLI_ASSOC);
 
 $jsonOutput = json_encode($result);
 
