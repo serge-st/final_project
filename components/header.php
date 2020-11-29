a78ca242e662691450ccd0eefa8bfde3583eb711
Sergejs Stecenko <sstecenko@evolution.com>
2020-11-29 23:22:25 +0200

    add meta tags

--- components/header.php
+++ components/header.php
@@ -7,6 +7,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="A simple TODO list LAMP App">
+    <meta name="keywords" content="todo app, todo, doit, lamp, serge-st, begginer webdev">
+    <meta name="Author" content="Serge St, traintocode@gmail.com">
+    <meta name="Copyright" content="Serge St">
+    <meta name="Address" content="https://github.com/serge-st">
+    <meta property="og:title" content="DOIT App">
+    <meta property="og:description" content="A simple TODO List App">
+    <meta property="og:url" content="https://doitapp.live/">
+    <meta property="og:image" content="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/263/direct-hit_1f3af.png" />
+
     <title>DOIT App</title>
     <link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/263/direct-hit_1f3af.png">
     <link rel="preconnect" href="https://fonts.gstatic.com">
